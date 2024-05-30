7451953cfd70a4dc157a98a9344b1315d1c1ed32
Christian Pschorr <pschorr.christian@gmail.com>
2024-05-30 12:22:53 +0200

    Quick Fixes für die Erstinstallation in TYPO3 v12

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,5 +6,5 @@ if(defined('TYPO3') === false) {
 
 call_user_func(function()	{
 	// @see: https://docs.typo3.org/p/georgringer/news/11.4/en-us/Tutorials/ExtendNews/ProxyClassGenerator/Index.html#2-register-the-class
-	$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News']['ps14_kist_news'] = 'kist_news';
+	$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News']['ps14_kist_news'] = 'ps14_kist_news';
 });
